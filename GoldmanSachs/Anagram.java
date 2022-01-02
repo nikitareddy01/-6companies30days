b467b50e3b1390c32b94df994627958ff55b2702
nikitareddy01 <43108647+nikitareddy01@users.noreply.github.com>
2022-01-02 15:23:38 +0530

    Add files via upload

---
+++ GoldmanSachs/Anagram.java
@@ -0,0 +1,56 @@
+package challenge;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Anagrams
+ */
+public class Anagram {
+    public static void main(String[] args) {
+        
+    
+     String[] word={"act","god","cat","dog","tac"};
+     System.out.println(Anagrams(word));
+
+ }
+ public static List<List<String>> Anagrams(String[] string_list) {
+    HashMap <String, List<String>> mp = new HashMap<>();
+     List<List<String>> ans = new ArrayList <List<String>>();
+     
+     for(String i: string_list)
+     {
+         char ch[] = i.toCharArray();
+         Arrays.sort(ch);
+         // Convert the string  
+         // e.g. cat->act, act->act, tac->act 
+         String s = String.valueOf(ch);
+         
+         // Check if that rep has 
+         // occurred already
+         if(mp.containsKey(s))
+         {
+             // If occurred add the original string to the map
+             mp.get(s).add(i);
+         }
+         
+         // If not present
+         else
+         {
+             // Create a new list
+             List<String> li = new ArrayList<>();
+             // Add the original string
+             li.add(i);
+             ans.add(li);
+             // Insert into the map the string as key 
+             // and the new list as value
+             mp.put(s,li);
+         }
+     }
+     
+     return ans;
+
+     
+}
+}
